eadd969d5f5173818a4986a68fd72a13fba743cf
fedelaborda <101400350+fedelaborda@users.noreply.github.com>
2022-05-17 14:28:15 -0300

    bahbfyae

---
+++ Assets/CubeMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    
+    public Vector3 cubeMovement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position += cubeMovement;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += cubeMovement;
+    }
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float movementSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+           transform.position += new Vector3(movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-movementSpeed, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, movementSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -movementSpeed, 0);
+        }
+    }
+}
